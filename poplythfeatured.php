0dc659dc119eaae2e894496b0c03ce28b150f0dd
lythium <niko.phoenix1@gmail.com>
2017-09-04 17:37:55 +0200

    update variable

--- poplythfeatured.php
+++ poplythfeatured.php
@@ -55,7 +55,7 @@ class PopLythFeatured extends Module {
         $product = $this->researchProduct($log);
         $have_image = ImageCore::hasImages($this->context->language->id, (int)$product["id_product"]);
         $cover = Product::getCover((int)$product["id_product"]);
-        die(var_dump($product));
+        // die(var_dump($product));
         // $id_cover = $product->id.'-'.$id_image;
 
         // Stock Variable
@@ -63,7 +63,7 @@ class PopLythFeatured extends Module {
            'product_select' => $product,
            'have_image' => (bool)$have_image,
            'id_cover' => (int)$cover["id_image"],
-           'product_name' => (string)$product->name[1],
+           'product_name' => (string)$product["name"],
         ));
 
         return $this->display(__FILE__, '/views/templates/hook/poplythfeatured.tpl');
@@ -78,7 +78,7 @@ class PopLythFeatured extends Module {
             // $result = New Product($result["id_product"]);
             // die(var_dump($result));
             if (empty($result)) {
-                $result = Product::getNewProducts($this->context->language->id,$page_number = 0, $nb_products = 10);
+                $result = Product::getNewProducts($this->context->language->id,$page_number = 0, $nb_products = 1);
                 // $sql = new DbQuery();
                 // $sql->select('p.id_product');
                 // $sql->from('product', 'p');
